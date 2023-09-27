84c09e598382ce09c6f3b70cba2798b1d5d7c75f
Amit Gupta <120751752+amit-gupta120@users.noreply.github.com>
2023-09-27 18:40:20 +0530

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,122 +1,120 @@
 import React from "react";
-import ShoppingImage from "../assets/reactShoppingApp.png";
-import todoImage from "../assets/reactTODO.jpg";
-import WeatherApp from "../assets/reactWeather.jpeg";
-import { Link } from "react-scroll";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import { Autoplay } from "swiper";
-// import { Pagination } from 'swiper/modules';
-
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
-
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
 
 const Portfolio = () => {
-  const portfolios = [
-    {
-      id: 1,
-      src: ShoppingImage,
-    },
-    {
-      id: 2,
-      src: todoImage,
-    },
-    {
-      id: 3,
-      src: WeatherApp,
-    },
-    {
-      id: 4,
-      src: ShoppingImage,
-    },
-    {
-      id: 5,
-      src: todoImage,
-    },
-    {
-      id: 6,
-      src: todoImage,
-    },
-  ];
   return (
-    <div 
+    <div
       name="portfolio"
-      className="w-full h-screen bg-gradient-to-b from-black to-gray-800 text-white"
+      className="w-full bg-gradient-to-b from-black to-gray-800 py-[7rem] px-4"
     >
-      {/* <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full"> */}
-      {/* <div className="p-4 mx-auto flex flex-col justify-center w-full h-full"> */}
-      <div className="customSliderContainerByAmit">
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
+      <div className="pb-8">
+        <p className="text-4xl ml-[155px] text-white font-bold inline border-b-4 border-gray-500">
+          Portfolio
+        </p>
+      </div>
+
+      <div className="max-w-[1024px] mx-auto grid md:grid-cols-3 gap-8">
+        <div className=" bg-gray-300 shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+          <h2 className="text-2xl font-bold text-center py-8 underline">
+            Cryptocurrency App
+          </h2>
+          <p className="text-center font-bold">Technology Used:</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">
+              ReactJs, Redux and Ant Design
+            </p>
+            <p className="py-2 border-b mx-8">
+              A app where you can see the Global Stats of the crypto currency
+              and can read news related to any currency and view the list of all
+              currency.
+            </p>
+          </div>
+          <div className="flex">
+            <button className="bg-[#00df9a] w-[120px] rounded-md font-medium my-6 mx-auto px-6 py-3">
+              <a
+                href="https://cryptocurrency-application.vercel.app/"
+                target="_blank"
+              >
+                Live
+              </a>
+            </button>
+            <button className="bg-[#00df9a] w-[120px] rounded-md font-medium my-6 mx-auto px-6 py-3">
+              <a
+                href="https://github.com/amit-gupta120/Cryptocurrency-Application"
+                target="_blank"
+              >
+                Git Repo
+              </a>
+            </button>
+          </div>
         </div>
-        {/* <div className="customSliderBoxByAmit"> */}
-        <Swiper
-          // install Swiper modules
-          modules={[Navigation, Pagination, Scrollbar, A11y]}
-          spaceBetween={60}
-          slidesPerView={4}
-          navigation
-          pagination={{ clickable: true }}
-          scrollbar={{ draggable: true }}
-          onSwiper={(swiper) => console.log(swiper)}
-          onSlideChange={() => console.log("slide change")}
-          style={{ backgroundColor: "#18202b",height:"600" }}
-        >
-          {portfolios.map(({ id, src }) => {
-            return (
-              <SwiperSlide key={src} style={{ width: 100, height: 250,backgroundColor:"#4b5563" }}>
-                <SwiperSlide key={src}>
-                  <div
-                    key={id}
-                    className="shadow-md shadow-gray-600 rounded-lg"
-                    style={{}}
-                  >
-                    <img
-                      src={src}
-                      alt=""
-                      className="rounded-md duration-200 hover:scale-120"
-                      style={{
-                        height: "185px",
-                        width: "100%",
-                        resizeMode: "contain",
-                      }}
-                    />
-                    <div className="flex items-center justify-center">
-                      <button className="w-1/2 m-4 duration-200  hover:scale-105">
-                        Demo
-                      </button>
-                      <button
-                        className="w-1/2 m-4 duration-200  hover:scale-105"
-                        onClick={() =>
-                          window.open(
-                            "https://github.com/your-username/your-repo",
-                            "_blank"
-                          )
-                        }
-                        style={{
-                          border: "none",
-                          background: "none",
-                          cursor: "pointer",
-                        }}
-                      >
-                        Code
-                      </button>
-                    </div>
-                  </div>
-                </SwiperSlide>
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-        {/* </div> */}
+
+        <div className="w-full shadow-xl bg-gray-300 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300">
+          <h2 className="text-2xl font-bold text-center py-8 underline">
+            Personal Portfolio
+          </h2>
+          <p className="text-center font-bold">Technology Used:</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">ReactJs and Tailwind CSS</p>
+            <p className="py-2 border-b mx-8">
+              In my portfolio, you'll find a collection of my work that reflects
+              my passion and dedication in creating meaningful and impactful
+              experiences.
+            </p>
+          </div>
+          <div className="flex ">
+            <button className="bg-[#00df9a] w-[120px] rounded-md font-medium my-6 mx-auto px-6 py-3">
+            <a
+                href="https://amitgupta.vercel.app/"
+                target="_blank"
+              >
+                Live
+              </a>
+            </button>
+            <button className="bg-[#00df9a] w-[120px] rounded-md font-medium my-6 mx-auto px-6 py-3">
+            <a
+                href="https://github.com/amit-gupta120/portfolio"
+                target="_blank"
+              >
+                Git Repo
+              </a>
+            </button>
+          </div>
+        </div>
+
+        <div className="w-full shadow-xl bg-gray-300 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300">
+          <h2 className="text-2xl font-bold text-center py-8 underline">Weather App</h2>
+          <p className="text-center font-bold">Technology Used:</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">HTML,CSS & JavaScript</p>
+            <p className="py-2 border-b mx-8">
+              In my portfolio, you'll find a collection of my work that reflects
+              my passion and dedication in creating meaningful and impactful
+              experiences.
+            </p>
+          </div>
+          <div className="flex ">
+          <button className="bg-[#00df9a] w-[120px] rounded-md font-medium my-6 mx-auto px-6 py-3">
+              <a
+                // href="https://cryptocurrency-application.vercel.app/"
+                target="_blank"
+              >
+                Live
+              </a>
+            </button>
+            <button className="bg-[#00df9a] w-[120px] rounded-md font-medium my-6 mx-auto px-6 py-3">
+              <a
+                href="https://github.com/amit-gupta120/Weather-App"
+                target="_blank"
+              >
+                Git Repo
+              </a>
+            </button>
+          </div>
         </div>
       </div>
+    </div>
   );
 };
+
 export default Portfolio;
+
